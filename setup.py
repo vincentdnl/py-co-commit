949acd337a5871b41adba84a93355fa0215dc8f6
Vincent Déniel <denielvincent@gmail.com>
2018-11-05 23:07:52 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,19 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='py_co_commit',
     version='0.1',
     description='An easy way to create a commit with multiple authors',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url='https://github.com/vincentdnl/py-co-commit.git',
     author='Vincent Déniel',
     author_email='vincentdnl@users.noreply.github.com',
     license='apache-2.0',
     packages=['py_co_commit'],
-    zip_safe=False,
     entry_points={
         'console_scripts': ['py_co_commit=py_co_commit:main'],
     }
